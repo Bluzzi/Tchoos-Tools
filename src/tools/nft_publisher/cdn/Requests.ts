b1fdc510204819e73fa3c11fd90ef8914d051b75
Bluzzi <camilledugas.pro@gmail.com>
2022-04-25 12:58:49 +0200

    ...

---
+++ src/tools/nft_publisher/cdn/Requests.ts
@@ -0,0 +1,13 @@
+const CDN_API_URL = "https://cdn.tchoos.pet/back_door/";
+
+const ROUTES: Routes = {
+    upload_animation: "upload_animation",
+    upload_model: "upload_model"
+}
+
+interface Routes {
+    upload_animation: string;
+    upload_model: string;
+}
+
+https://www.typescriptlang.org/docs/handbook/enums.html
